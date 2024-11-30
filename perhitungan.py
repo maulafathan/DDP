9b0b941ed9ed038be1982ee31e95913066be482f
maulafathan <maulafathanlubis@gmail.com>
2024-12-01 00:07:14 +0700

    Add files via upload
    upload tugas ddp10

---
+++ perhitungan.py
@@ -0,0 +1,21 @@
+import math
+
+def tambah(bil1, bil2):
+    hasil = 2 + 8
+    print("Hasil Tambah Dari",2, "+", 8, "=", hasil)
+
+def kurang(bil1, bil2):
+    hasil = 16 - 6
+    print("Hasil Pengurangan Dari",16, "-", 6, "=", hasil)
+
+def kali(bil1, bil2):
+    hasil = 4 * 4
+    print("Hasil Perkalian Dari",4, "*", 4, "=", hasil)
+
+def bagi(bil1, bil2):
+    hasil = 10 / 2
+    print("Hasil Pembagian Dari",10, "/", 2, "=", hasil)
+
+def pangkat(bil1, bil2):
+    hasil = math.pow(8, 2)
+    print("Hasil Pemangkatan Dari",8, "^", 2, "=", hasil)
